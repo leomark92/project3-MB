eee57586fba8fed8f1887ca78699b30e2bc63f41
leomark92 <markobogush@gmail.com>
2018-01-23 15:09:53 +0200

    adding landing2

---
+++ landing2/js/common.js
@@ -0,0 +1,9 @@
+$('.mobile_btn').click(function(){
+    $('.navbar').toggleClass('display_m');
+    
+})
+
+$('.mobile_btn').click(function(){
+    $('.mobile_btn').toggleClass('transform_b');
+})
+
